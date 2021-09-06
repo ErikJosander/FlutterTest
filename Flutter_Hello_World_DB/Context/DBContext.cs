aec11b89cc05598a709eceafab2bc5751e3b3d9e
Josander <32512932+ErikJosander@users.noreply.github.com>
2021-09-06 15:52:50 +0200

    awd

---
+++ Flutter_Hello_World_DB/Context/DBContext.cs
@@ -0,0 +1,17 @@
+﻿using Flutter_Hello_World_DB.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Flutter_Hello_World_DB.Context
+{
+    public class DBContext : IdentityDbContext<User>
+    {
+        public DBContext(DbContextOptions<DbContext> options)
+        {
+
+        }
+
+        
+    }
+}
---
+++ Flutter_Hello_World_DB/Entities/LinksInPage.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace Flutter_Hello_World_DB.Entities
+{
+    public class LinksInPage
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public HttpWebRequest httpWebRequest { get; set; }
+    }
+}
