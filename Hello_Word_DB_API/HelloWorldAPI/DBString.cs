82d4f6817f2e69d53f71d1a0c4f027bb00ce5c8a
Josander <32512932+ErikJosander@users.noreply.github.com>
2021-09-06 23:12:43 +0200

    Created First Files

---
+++ Hello_Word_DB_API/HelloWorldAPI/DBString.cs
@@ -0,0 +1,7 @@
+﻿namespace HelloWorldAPI
+{
+    public class DBString
+    {
+        public static string localDbString = @"Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;";
+    }
+}
--- Hello_Word_DB_API/HelloWorldAPI/Program.cs
+++ Hello_Word_DB_API/HelloWorldAPI/Program.cs
@@ -1,11 +1,9 @@
+using HelloWorldDB.Context;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace HelloWorldAPI
 {
@@ -13,7 +11,25 @@ namespace HelloWorldAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    var context = scope.ServiceProvider.GetService<HelloWorldDBContext>();
+                    // for demo purposes, delete the database & migrate on startup so 
+                    // we can start with a clean slate
+                    context.Database.EnsureDeleted();
+                    context.Database.EnsureCreated();
+                }
+
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating the database.");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
