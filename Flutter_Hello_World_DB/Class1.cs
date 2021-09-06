024f28a417771e374f82cdb23c78e162179a59d8
Josander <32512932+ErikJosander@users.noreply.github.com>
2021-09-06 14:24:16 +0200

    added user

--- Flutter_Hello_World_DB/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Flutter_Hello_World_DB
-{
-    public class Class1
-    {
-    }
-}
---
+++ Flutter_Hello_World_DB/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
+
+namespace Flutter_Hello_World_DB.Entities
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        [MaxLength(50)]
+        public override string UserName { get; set; }
+    }
+}
