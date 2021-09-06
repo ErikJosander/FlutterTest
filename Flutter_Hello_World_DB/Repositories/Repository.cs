58bf80de622f3055a0b9851dc463b24903dda18f
Josander <32512932+ErikJosander@users.noreply.github.com>
2021-09-06 16:38:45 +0200

    Added services and repositories

---
+++ Flutter_Hello_World_DB/Repositories/Repository.cs
@@ -0,0 +1,63 @@
+﻿using Flutter_Hello_World_DB.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flutter_Hello_World_DB.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        public void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRage(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
